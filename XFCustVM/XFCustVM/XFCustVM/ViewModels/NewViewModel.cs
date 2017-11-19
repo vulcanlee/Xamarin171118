ba369e54a52535802aec9b8715295892221f2fc2
Vulcan <vulcan.lee@gmail.com>
2017-11-19 12:14:32 +0800

    自訂ViewModel類別練習

---
+++ XFCustVM/XFCustVM/XFCustVM/ViewModels/NewViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XFCustVM.ViewModels
+{
+
+    public class NewViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Title { get; set; } = "New Main Page";
+        private readonly INavigationService _navigationService;
+
+        public NewViewModel()
+        {
+
+        }
+        public NewViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+
+    }
+
+}
---
+++ XFCustVM/XFCustVM/XFCustVM/Views/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XFCustVM.ViewModels;
+
+namespace XFCustVM.Views
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+
+            //var foo = new NewViewModel();
+            //this.BindingContext = foo;
+        }
+
+        public void OnChanged(object s, EventArgs e)
+        {
+            NewViewModel bar = this.BindingContext as NewViewModel;
+            bar.Title = "Change Title to New Name";
+        }
+    }
+}
