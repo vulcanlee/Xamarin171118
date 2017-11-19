0d083a9cf988e5cf1aa9f77cbc5b0b48f82b6545
Vulcan <vulcan.lee@gmail.com>
2017-11-19 12:15:20 +0800

    Xamarin.Forms原生專案練習 使用Prism+MVVM開發方法

---
+++ XFPrismMVVM/XFPrismMVVM/XFPrismMVVM/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XFPrismMVVM.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Title { get; set; }
+        public string MyEntry { get; set; }
+        public string MyLabel { get; set; }
+        public DelegateCommand MyButtonCommand { get; set; }
+        public MainPageViewModel()
+        {
+            Title = "Main Page";
+
+            MyButtonCommand = new DelegateCommand(() =>
+            {
+                MyLabel = MyEntry;
+            });
+        }
+
+    }
+}
