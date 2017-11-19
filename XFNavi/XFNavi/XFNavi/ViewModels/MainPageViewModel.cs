dd4545996b284f570d13d8b5c654f2840e3fde2b
Vulcan <vulcan.lee@gmail.com>
2017-11-19 12:09:24 +0800

    頁面導航(透過導航頁面)

---
+++ XFNavi/XFNavi/XFNavi/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XFNavi.ViewModels
+{
+
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DelegateCommand GoNextPageCommand { get; set; }
+
+        private readonly INavigationService _navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            GoNextPageCommand = new DelegateCommand(async () =>
+            {
+                await _navigationService.NavigateAsync("NextPage");
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+
+    }
+
+}
