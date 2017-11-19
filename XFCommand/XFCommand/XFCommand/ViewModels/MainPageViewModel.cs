5bd677912323d86a428590d97d2e7284a73a848a
Vulcan <vulcan.lee@gmail.com>
2017-11-19 12:12:48 +0800

    命令綁定與參數

---
+++ XFCommand/XFCommand/XFCommand/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XFCommand.ViewModels
+{
+
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string MyLabel { get; set; }
+        public DelegateCommand<string> ButtonCommand { get; set; }
+
+        private readonly INavigationService _navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            ButtonCommand = new DelegateCommand<string>(x =>
+              {
+                  MyLabel = x;
+              });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+
+    }
+
+}
