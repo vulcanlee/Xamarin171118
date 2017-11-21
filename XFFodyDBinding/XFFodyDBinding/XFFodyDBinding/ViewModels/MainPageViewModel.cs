dca5b8abe90e5b6689025a0a20ad409184ba7e3e
李進興 Vulcan <vulcan@miniasp.com>
2017-11-22 01:14:54 +0800

    各種資料綁定實作方法 – 使用 PropertyChanged.Fody

---
+++ XFFodyDBinding/XFFodyDBinding/XFFodyDBinding/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XFFodyDBinding.ViewModels
+{
+
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string YourName { get; set; }
+        public string YourAnswer { get; set; }
+
+        public DelegateCommand LoginCommand { get; set; }
+
+        private readonly INavigationService _navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            LoginCommand = new DelegateCommand(() =>
+            {
+                YourAnswer = "XF : " + YourName;
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+
+    }
+}
