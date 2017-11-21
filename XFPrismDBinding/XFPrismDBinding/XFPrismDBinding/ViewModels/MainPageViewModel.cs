65718a7ad2986afcad7f51bd74b35c26b3b73411
李進興 Vulcan <vulcan@miniasp.com>
2017-11-22 00:38:39 +0800

    各種資料綁定實作方法 – 使用 Prism 類別

---
+++ XFPrismDBinding/XFPrismDBinding/XFPrismDBinding/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XFPrismDBinding.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private string _YourName;
+
+        public string YourName
+        {
+            get { return _YourName; }
+            set { SetProperty(ref _YourName, value); }
+        }
+
+
+        #region YourAnswer
+        private string _YourAnswer;
+        /// <summary>
+        /// YourAnswer
+        /// </summary>
+        public string YourAnswer
+        {
+            get { return this._YourAnswer; }
+            set { this.SetProperty(ref this._YourAnswer, value); }
+        }
+        #endregion
+
+
+        public DelegateCommand LoginCommand { get; set; }
+
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+
+            LoginCommand = new DelegateCommand(() =>
+            {
+                YourAnswer = "XF : " + YourName;
+            });
+        }
+    }
+}
