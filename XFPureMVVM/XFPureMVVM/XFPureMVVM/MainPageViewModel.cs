db39348bc31b2d4085c3de56e236aae308624c86
李進興 Vulcan <vulcan@miniasp.com>
2017-11-22 13:18:11 +0800

    Xamarin.Forms 專案開發方式說明 - 使用 XAML 定義 UI、使用 ViewModel 寫邏輯

---
+++ XFPureMVVM/XFPureMVVM/XFPureMVVM/MainPageViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XFPureMVVM
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _MyEntry;
+
+        public string MyEntry
+        {
+            get { return _MyEntry; }
+            set
+            {
+                if (_MyEntry != value)
+                {
+                    _MyEntry = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _MyLabel;
+
+        public string MyLabel
+        {
+            get { return _MyLabel; }
+            set
+            {
+                if (_MyLabel != value)
+                {
+                    _MyLabel = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public ICommand MyButtonCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            MyButtonCommand = new Command(() =>
+            {
+                MyLabel = MyEntry;
+            });
+        }
+
+        void OnPropertyChanged(
+                [CallerMemberName]string propertyName = "")
+        {
+            var fooPropertyChanged = PropertyChanged;
+            if (fooPropertyChanged != null)
+            {
+                PropertyChanged(this,
+                              new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
