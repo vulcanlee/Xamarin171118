1ea3592e0d31c4fc31f5219e1b9fed1279ef34fa
李進興 Vulcan <vulcan@miniasp.com>
2017-11-22 13:19:29 +0800

    Xamarin.Forms 專案開發方式說明 - 使用 Prism 框架，快速建立專案

---
+++ XFPurePrism/XFPurePrism/XFPurePrism/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XFPurePrism.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+
+        #region MyEntry
+        private string _MyEntry;
+        /// <summary>
+        /// MyEntry
+        /// </summary>
+        public string MyEntry
+        {
+            get { return this._MyEntry; }
+            set { this.SetProperty(ref this._MyEntry, value); }
+        }
+        #endregion
+
+
+        #region MyLabel
+        private string _MyLabel;
+        /// <summary>
+        /// MyLabel
+        /// </summary>
+        public string MyLabel
+        {
+            get { return this._MyLabel; }
+            set { this.SetProperty(ref this._MyLabel, value); }
+        }
+        #endregion
+
+        public DelegateCommand MyButtonCommand { get; set; }
+
+        public MainPageViewModel(INavigationService navigationService) 
+            : base (navigationService)
+        {
+            Title = "Main Page";
+
+            MyButtonCommand = new DelegateCommand(() =>
+            {
+                MyLabel = MyEntry;
+            });
+        }
+    }
+}
