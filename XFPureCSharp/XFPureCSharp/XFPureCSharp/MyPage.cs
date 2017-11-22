1b737a5eb173b9f4f30adeb5d0c0183757005eda
李進興 Vulcan <vulcan@miniasp.com>
2017-11-22 13:14:20 +0800

    Xamarin.Forms 專案開發方式說明 - 全部都使用 C# 程式碼來開發

---
+++ XFPureCSharp/XFPureCSharp/XFPureCSharp/MyPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFPureCSharp
+{
+    public class MyPage : ContentPage
+    {
+        Entry MyEntry;
+        Label MyLabel;
+        Button MyButton;
+        public MyPage()
+        {
+            MyEntry = new Entry();
+            MyLabel = new Label();
+            MyButton = new Button();
+            this.Content = new StackLayout()
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Children =
+                {
+                    MyEntry,MyLabel,MyButton
+                }
+            };
+
+            MyEntry.Placeholder = "請輸入你的答案";
+            MyButton.Text = "送出";
+            MyButton.Clicked += MyButton_Clicked;
+        }
+
+        private void MyButton_Clicked(object sender, EventArgs e)
+        {
+            MyLabel.Text = MyEntry.Text;
+        }
+    }
+}
