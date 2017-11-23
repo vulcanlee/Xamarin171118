e10887cc6a8339bfd5f398706e335dd0ea41fd78
李進興 Vulcan <vulcan@miniasp.com>
2017-11-23 16:42:59 +0800

    Xamarin.Forms / .NET Standard 體驗之旅 4 : QR Code 條碼掃描

--- XFQRCode/Solution/XFQRCode/XFQRCode.Android/MainActivity.cs
+++ XFQRCode/Solution/XFQRCode/XFQRCode.Android/MainActivity.cs
@@ -4,6 +4,13 @@ using Android.OS;
 using Microsoft.Practices.Unity;
 using Prism.Unity;
 
+// [UsesPermission] attribute: 
+//  You can now specify the Android permissions your app requires with an assembly level attribute. 
+// This is analogous to adding a <uses-permission> element to the AndroidManifest.xml.
+// https://developer.xamarin.com/releases/android/mono_for_android_4/mono_for_android_4.0.0/
+[assembly: UsesPermission(Android.Manifest.Permission.Flashlight)]
+[assembly: UsesPermission(Android.Manifest.Permission.Camera)]
+
 namespace XFQRCode.Droid
 {
     [Activity(Label = "XFQRCode", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
