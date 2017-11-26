1643f1ff513b39e1bfab1136e84ae0553503e365
Vulcan <vulcan.lee@gmail.com>
2017-11-26 10:03:27 +0800

    進階應用 Font Awesome

---
+++ XFCustFont/XFCustFont/XFCustFont.Android/Renderers/FontAwesomeLabelRenderer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using Android.Graphics;
+
+
+[assembly: ExportRenderer(typeof(XFCustFont.CustomControls.FontAwesomeLabel), typeof(XFCustFont.Droid.Renderers.FontAwesomeLabelRenderer))]
+namespace XFCustFont.Droid.Renderers
+{
+    public class FontAwesomeLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            var label = Control;
+            Typeface font;
+            try
+            {
+                font = Typeface.CreateFromAsset(Forms.Context.Assets, "fontawesome.ttf");
+                label.Typeface = font;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("TTF file not found. Make sure the Android project contains it at 'fontawesome.ttf'.");
+            }
+
+        }
+    }
+}
---
+++ XFCustFont/XFCustFont/XFCustFont/CustomControls/FontAwesomeLabel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XFCustFont.CustomControls
+{
+    public class FontAwesomeLabel : Label
+    {
+        public FontAwesomeLabel()
+        {
+            FontFamily = Device.OnPlatform("fontawesome", "fontawesome", "/Assets/Fonts/fontawesome.ttf#FontAwesome");
+        }
+    }
+}
