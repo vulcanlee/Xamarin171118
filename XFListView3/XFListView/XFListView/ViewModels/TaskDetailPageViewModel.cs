75d13acad0d32f7bafbde2efe134501d9ea45de8
Vulcan <vulcan.lee@gmail.com>
2017-11-26 16:02:13 +0800

    集合資料CRUD 新增記錄

---
+++ XFListView3/XFListView/XFListView/ViewModels/TaskDetailPageViewModel.cs
@@ -0,0 +1,69 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using XFListView.Models;
+
+namespace XFListView.ViewModels
+{
+
+    public class TaskDetailPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public MyTaskItem MyTaskItemSelected { get; set; } = new MyTaskItem();
+        private readonly INavigationService _navigationService;
+
+        public DelegateCommand DeleteCommand { get; set; }
+        public DelegateCommand SaveCommand { get; set; }
+
+        public TaskDetailPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            DeleteCommand = new DelegateCommand(async () =>
+            {
+                var fooPara = new NavigationParameters();
+                fooPara.Add("Action", "D");
+                fooPara.Add("ActionRec", MyTaskItemSelected);
+                await _navigationService.GoBackAsync(fooPara);
+            });
+            SaveCommand = new DelegateCommand(async () =>
+            {
+                var fooPara = new NavigationParameters();
+                fooPara.Add("Action", RecordActionSymbol);
+                fooPara.Add("ActionRec", MyTaskItemSelected);
+                await _navigationService.GoBackAsync(fooPara);
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+        public string RecordActionSymbol { get; set; }
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("Record"))
+            {
+                MyTaskItemSelected = parameters["Record"] as MyTaskItem;
+            }
+
+            RecordActionSymbol = "";
+            if (parameters.ContainsKey("Action"))
+            {
+                RecordActionSymbol = parameters["Action"] as string;
+            }
+
+
+        }
+
+    }
+}
