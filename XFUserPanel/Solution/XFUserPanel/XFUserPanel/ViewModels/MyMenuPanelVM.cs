6f1cf22c5048a2b1d23a200fd3dd0f4aa6646e8e
李進興 Vulcan <vulcan@miniasp.com>
2017-11-28 22:30:27 +0800

    進行 Entry 控制項客製化(Renderer)

---
+++ XFUserPanel/Solution/XFUserPanel/XFUserPanel/ViewModels/MyMenuPanelVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XFUserPanel.ViewModels
+{
+    public class MyMenuPanelVM : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Color PanelColor { get; set; }
+        public MyMenuButtonVM Button1 { get; set; } = new MyMenuButtonVM();
+        public MyMenuButtonVM Button2 { get; set; } = new MyMenuButtonVM();
+        public MyMenuButtonVM Button3 { get; set; } = new MyMenuButtonVM();
+        public MyMenuButtonVM Button4 { get; set; } = new MyMenuButtonVM();
+        public MyMenuButtonVM Button5 { get; set; } = new MyMenuButtonVM();
+
+    }
+}
