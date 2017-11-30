0fdd6fdf901b2709263f07c96750e9bd622dc80c
李進興 Vulcan <vulcan@miniasp.com>
2017-11-30 18:57:26 +0800

    使用 PCLStorage 儲存到手機上

---
+++ XFPCLFile/Starter/XFPCLFile/XFPCLFile/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using XFPCLFile.Models;
+
+namespace XFPCLFile.ViewModels
+{
+
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public MyPageData PageData { get; set; } = new MyPageData();
+        public DelegateCommand SaveCommand { get; set; }
+        public DelegateCommand LoadCommand { get; set; }
+        private readonly INavigationService _navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            SaveCommand = new DelegateCommand(async () =>
+            {
+            });
+            LoadCommand = new DelegateCommand(async () =>
+            {
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+
+    }
+}
