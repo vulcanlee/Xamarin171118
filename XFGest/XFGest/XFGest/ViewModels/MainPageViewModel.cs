6b92f6bf1bfe021d840b1c1f9ffb11401c483419
Vulcan <vulcan.lee@gmail.com>
2017-12-02 23:23:59 +0800

    手勢操作

---
+++ XFGest/XFGest/XFGest/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XFGest.ViewModels
+{
+
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Title { get; set; }
+
+        public DelegateCommand BoxViewCommand { get; set; }
+        public DelegateCommand LabelCommand { get; set; }
+
+        private readonly INavigationService _navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            BoxViewCommand = new DelegateCommand(() =>
+            {
+                Title = "BoxView Tapped";
+            });
+            LabelCommand = new DelegateCommand(() =>
+            {
+                Title = "Label Tapped";
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+
+    }
+}
