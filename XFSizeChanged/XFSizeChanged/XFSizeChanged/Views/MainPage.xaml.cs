140d400eeaf859d6cefcd5c53af939bb07a24534
Vulcan <vulcan.lee@gmail.com>
2017-12-02 23:21:27 +0800

    手機螢幕旋轉的事件

---
+++ XFSizeChanged/XFSizeChanged/XFSizeChanged/Views/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XFSizeChanged.ViewModels;
+
+namespace XFSizeChanged.Views
+{
+	public partial class MainPage : ContentPage
+	{
+        MainPageViewModel fooMainPageViewModel;
+        public MainPage ()
+		{
+			InitializeComponent ();
+
+            fooMainPageViewModel = this.BindingContext as MainPageViewModel;
+            this.SizeChanged += MainPage_SizeChanged;
+		}
+
+        private void MainPage_SizeChanged(object sender, EventArgs e)
+        {
+            if(this.Width < this.Height)
+            {
+                fooMainPageViewModel.MyFontSize = 20;
+            }
+            else
+            {
+                fooMainPageViewModel.MyFontSize = 40;
+            }
+        }
+    }
+}
